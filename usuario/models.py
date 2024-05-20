d6088b84c378c7a5bebec0f1f6cf0a2853011ebf
Mirko Iriarte <20184037@aloe.ulima.edu.pe>
2024-05-19 20:36:29 -0500

    Update usuario models (usuuario, profesor, estudiante)

--- usuario/models.py
+++ usuario/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+# Usuario
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    contraseña = models.CharField(max_length=100)
+    codigo = models.CharField(max_length=100)
+
+# Profesor
+class Profesor(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+
+# Estudiante
+class Estudiante(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
