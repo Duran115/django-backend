b5aaac6389545ea28bae2dda75d9510eb2acf155
Mirko Iriarte <20184037@aloe.ulima.edu.pe>
2024-05-26 14:32:23 -0500

    Add  estudiante serializer.py

---
+++ estudiante/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from rest_framework import routers
+from estudiante import views
+
+# Versionado de la API
+router = routers.DefaultRouter()
+router.register(r'estudiantes', views.EstudianteViewSet)
+
+urlpatterns = [
+    path('api/v1/', include(router.urls)),
+]
--- estudiante/views.py
+++ estudiante/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializer import EstudianteSerializer
+from .models import Estudiante
 
-# Create your views here.
+class EstudianteViewSet(viewsets.ModelViewSet):
+    serializer_class = EstudianteSerializer
+    queryset = Estudiante.objects.all()
