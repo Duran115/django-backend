3b636e8c2a403322fbb967658c6b3411a0b16ce7
Mirko Iriarte <20184037@aloe.ulima.edu.pe>
2024-05-26 04:30:02 -0500

    Update admin.py

--- curso/admin.py
+++ curso/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import Curso
 
-admin.site.register(Curso)
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'profesor', 'mostrar_estudiantes_matriculados']
+
+    def mostrar_estudiantes_matriculados(self, obj):
+        estudiantes = ', '.join([str(estudiante) for estudiante in obj.estudiantes_matriculados])
+        return estudiantes if estudiantes else '-'
+
+    mostrar_estudiantes_matriculados.short_description = 'Estudiantes Matriculados'
+
+admin.site.register(Curso, CursoAdmin)
